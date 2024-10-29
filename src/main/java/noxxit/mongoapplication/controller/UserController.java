c7b4791d9209c43141be06124ee8fc265749e138
EliasPires <elias.cpires@gmail.com>
2024-10-29 14:45:04 -0300

    Usando padrão DTO para retornar usuários.

--- src/main/java/noxxit/mongoapplication/controller/UserController.java
+++ src/main/java/noxxit/mongoapplication/controller/UserController.java
@@ -1,6 +1,7 @@
 package noxxit.mongoapplication.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import noxxit.mongoapplication.dto.UserDTO;
 import noxxit.mongoapplication.model.User;
 import noxxit.mongoapplication.service.UserService;
 
@@ -20,9 +22,10 @@ public class UserController {
     
 
     @RequestMapping(method =RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
       
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
