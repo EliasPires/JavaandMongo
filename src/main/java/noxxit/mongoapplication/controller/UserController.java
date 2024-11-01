f4ff27ee7b0ff92b97c1b6748d974455ff8323d7
EliasPires <elias.cpires@gmail.com>
2024-11-01 11:29:25 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/noxxit/mongoapplication/controller/UserController.java
+++ src/main/java/noxxit/mongoapplication/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import noxxit.mongoapplication.dto.UserDTO;
+import noxxit.mongoapplication.model.Post;
 import noxxit.mongoapplication.model.User;
 import noxxit.mongoapplication.service.UserService;
 
@@ -65,6 +66,14 @@ public class UserController {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+ 
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
